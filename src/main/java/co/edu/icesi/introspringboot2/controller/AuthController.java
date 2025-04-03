62e4682b34f0cf6b28a8e91d97971d7bd1471fed
Domiciano Rincón <domiciano.rincon@correounivalle.edu.co>
2025-04-03 09:26:37 -0500

    Semana 9

--- src/main/java/co/edu/icesi/introspringboot2/controller/AuthController.java
+++ src/main/java/co/edu/icesi/introspringboot2/controller/AuthController.java
@@ -1,16 +1,31 @@
 package co.edu.icesi.introspringboot2.controller;
 
+import co.edu.icesi.introspringboot2.entity.User;
+import co.edu.icesi.introspringboot2.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthController {
 
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/signup")
-    public String signup() {
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
         return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signupCreate(@ModelAttribute User user) {
+        userService.createUser(user);
+        return "redirect:/login";
     }
 
     @GetMapping("/login")
